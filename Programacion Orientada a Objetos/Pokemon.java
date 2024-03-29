c493330785e02f78551d4f498b37c87863e11ed3
Valency12 <109124439+Valency12@users.noreply.github.com>
2024-03-28 23:21:30 -0600

    Add files via upload

---
+++ Programacion Orientada a Objetos/Pokemon.java
@@ -0,0 +1,20 @@
+class Pokemon {
+    String nombre = newString();
+    String[] tipos = new String[];
+    String[] abilities = new String[2];
+    int[] stats;
+
+    void atacar() {
+        System.out.println("Estoy atacando :3");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Pokemon magnamite = new Pokemon();
+        Pokemon applin = new Pokemon();
+
+        magnamite.atacar();
+        applin.atacar():
+    }
+}
